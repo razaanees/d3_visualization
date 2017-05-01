9563f56d0331b95bb021db451784ae4163f83e5d
Raza Anees <razanees@hotmail.com>
2017-04-30 19:27:08 -0600

    Add initial design and update README
    Initial design made in html, css, js files and all relevant details
    filled out in the README.md file

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # d3_visualization
+
+* Summary
+The Sustainable Development Solutions Network (SDSN) has created global happiness reports for the past several years. These reports allots happiness scores to 148 countries based on six factors: GDP per capita, social support and mobility, health, trust in government, freedom, and generosity. This visualization shows the change in these score from the year 2015 to 2017.
+
+* Design
+    1. Chart type: After EDA of the world happiness data, I decided to present
+       the change in happiness (Happiness Score) of all of the countries from 2015 to 2017. A simple and effective way of doing this is using Edward
+       Tufte's Slopegraph
+
+    2. Visual Encodings:
+        * Country: Shape (one line for each country)
+        * Happiness score: Planar (y-axis)
+        * Year: Planar (x-axis)
+        * Change in happiness: Slope
+
+    3. Layout: The Slopegraph has three vertical invisible lines spaced evenly
+               apart horizontally representing the three years (2015, 2016, 2017). Each country is represented as a small dot on each of those lines. The positions of the countries are determined by their Happiness Score. The highest Happiness Score is at the top and each score is displayed next to the country's dot. Each country's Happiness score is connected with a line between the three years. This line forms the slope that encodes change in happiness.
+
+               The user can toggle on/off specific countries by clicking named buttons on the top left corner of the page or the country's slope. The user can also toggle off the year 2016 to make direct comparisons between 2015 and 2017.
+
+* Feedback
+N/A for initial design
+
+* Resources
+    * [World Happiness report: Helliwell, J., Layard, R., & Sachs, J. (2017). World Happiness Report 2017, New York: Sustainable Development Solutions Network.](http://worldhappiness.report/)
+    * [2015 - 2016 World happiness data](https://www.kaggle.com/unsdsn/world-happiness)
+    * [Slopegraph - Sundar's Block](https://bl.ocks.org/eesur/ee8d0ab88229d0e07f7f)
+    * [Stack Overflow](https://www.stackoverflow.com)
+    * [Udacity Data Analyst ND](https:www.udacity.com)
+    * [Reusable D3 syntax](https://medium.freecodecamp.com/a-gentle-introduction-to-d3-how-to-build-a-reusable-bubble-chart-9106dc4f6c46)
---
+++ js/main.js
@@ -0,0 +1,242 @@
+// Multiple slogegraph chart code
+
+//function js() {
+    //'use strict';
+
+var slopey = function() {
+    "use strict";
+
+    var w = 200, // width
+        h = 600,
+        margin = {top: 40, bottom: 40, left: 80, right: 80},
+        gutter = 50,
+        strokeColour = 'black',
+        keyValues = [],
+        keyName = '',
+        format = d3.format(''),
+        sets;
+
+    var dispatch = d3.dispatch('_hover');
+
+    var svg, yScale;
+
+    function exports(_selection) {
+        _selection.each(function(data) {
+
+            // format and clean data
+            var keyExtent = d3.extent(data, function(d){
+                return d["2017"];
+            });
+            // adjust screen width for number of sets
+            w = w * keyValues.length;
+            // get number of sets
+            sets = keyValues.length - 1;
+            // use same scale for all sides
+            yScale = d3.scale.linear()
+                .domain(keyExtent)
+                .range([h-margin.top, margin.bottom+8]);
+
+            // reset graph
+            d3.select(this).select('svg').remove();
+
+            svg = d3.select(this)
+                .append('svg')
+                .attr('width', w)
+                .attr('height', h);
+
+            render(data,0);
+        });
+    }
+
+    // apply each set of scores and start/end labels at the end
+    function render(data, n) {
+        if (n < keyValues.length-1) {
+            lines(data, n);
+            middleLabels(data, n);
+            return render(data, n+1);
+        }
+        else {
+            startLabels(data, n);
+            endLabels(data, n);
+            return n;
+        }
+    }
+
+    // connecting lines
+    function lines (data, n) {
+
+        var lines = svg.selectAll('.s-line-'+n)
+            .data(data)
+            .enter().append('line');
+
+        lines.attr({
+            x1: function () {
+                if (n===0) {
+                    return margin.left;
+                }
+                else {
+                    return ((w/sets)*n) + margin.left/2;
+                }
+            },
+            y1: function(d) {return yScale(d[keyValues[n]]);},
+            x2: function () {
+                if (n === sets-1) {
+                    return w - margin.right;
+                }
+                else {
+                    return ((w/sets)*(n+1)) - gutter;
+                }
+            },
+            y2: function(d) {return yScale(d[keyValues[n+1]]);},
+            stroke:  strokeColour,
+            'stroke-width': 1,
+            class: function(d, i) {return 'elm s-line-' + n + ' sel-' + i;}
+        })
+        .on('mouseover', dispatch._hover);
+    }
+
+    // start labels to be applied to the very left of chart
+    function startLabels(data) {
+
+        var startLabels = svg.selectAll('.l-labels')
+            .data(data)
+            .enter()
+            .append('text')
+            .attr({
+                class: function(d,i) {return 'labels l-labels elm sel-'+i},
+                x: margin.left - 3,
+                y: function (d) {return yScale(d[keyValues[0]])+4;}
+            })
+            .text(function (d) {
+                return d[keyName] + ' ' + format(d[keyValues[0]])
+            })
+            .style('text-anchor', 'end')
+            .on('mouseover', dispatch._hover);
+
+        // year title
+        svg.append('text')
+            .attr({
+                class: 's-title',
+                x: margin.left - 3,
+                y: margin.top/2 - 4
+            })
+            .text(keyValues[0] + "↓")
+            .style('text-anchor', 'end');
+    }
+
+    function middleLabels(data, n) {
+
+        if (n !== sets-1) {
+            var middleLabels = svg.selectAll('.m-labels-' + n)
+                .data(data)
+                .enter()
+                .append('text')
+                .attr({
+                    class: function(d,i) {return 'labels m-labels-' + n +' elm'+' sel-'+i;},
+                    x: ((w/sets)*(n+1)) + 15,
+                    y: function(d) {return yScale(d[keyValues[n+1]]) + 4;}
+                })
+                .text(function(d) {return format(d[keyValues[n+1]]);})
+                .style('text-anchor', 'end')
+                .on('mouseover', dispatch._hover);
+
+        // year title at the top of the labels
+        svg.append('text')
+            .attr({
+                class: 'm-title',
+                x: ((w/sets)*(n+1)) +15,
+                y: margin.top/2 - 4
+            })
+            .text(function(d) {
+                return [keyValues[n+1]] + "↓";
+            })
+            .style('text-anchor', 'end');
+        }
+    }
+
+    function endLabels(data) {
+
+        // get the last year in the data list-style
+        var e = keyValues.length - 1;
+
+        var endLabels = svg.selectAll('.e-labels-')
+            .data(data).enter()
+            .append('text')
+            .attr({
+                class: function(d,i) {
+                    return "labels e-labels- elm sel-" + i;
+                },
+                x: w-margin.right + 4,
+                y: function(d) {return yScale(d[keyValues[e]]);}
+            })
+            .text(function(d) {
+                return d[keyName] + " " + format(d[keyValues[e]]);
+            })
+            .style('text-anchor', 'start')
+            .on('mouseover', dispatch._hover);
+
+        // year title on top
+        svg.append('text')
+            .attr({
+                class: 'e-title',
+                x: w-margin.right + 4,
+                y: margin.top/2 - 4
+            })
+            .text(function (d){
+                return keyValues[e] + " ↓";
+            })
+            .style("text-anchor", "start")
+    }
+
+    exports.w = function(value) {
+        if(!arguments.length) return w;
+        w = value;
+        return this;
+    }
+
+    exports.h = function(value) {
+        if(!arguments.length) return h;
+        h = value;
+        return this;
+    }
+
+    exports.margin = function(value) {
+        if(!arguments.length) return margin;
+        margin = value;
+        return this;
+    }
+
+    exports.gutter = function(value) {
+        if(!arguments.length) return gutter;
+        gutter = value;
+        return this;
+    }
+
+    exports.format = function(value) {
+        if(!arguments.length) return format;
+        format = value;
+        return this;
+    }
+
+    exports.strokeColour = function(value) {
+        if(!arguments.length) return strokeColour;
+        strokeColour = value;
+        return this;
+    }
+
+    exports.keyValues = function(value) {
+        if(!arguments.length) return keyValues;
+        keyValues = value;
+        return this;
+    }
+
+    exports.keyName = function(value) {
+        if(!arguments.length) return keyName;
+        keyName = value;
+        return this;
+    }
+
+    d3.rebind(exports, dispatch, 'on');
+    return exports;
+
+};
